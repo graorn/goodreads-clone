2405a2ad5d4c91725a10da2189d5ef1c554201fe
Jovan Djukic <jovandjukic2@gmail.com>
2018-07-23 16:30:02 +0200

    Added unique book in reading list

--- app/models/reading_list.rb
+++ app/models/reading_list.rb
@@ -1,4 +1,6 @@
 class ReadingList < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates_uniqueness_of :book
 end
--- db/migrate/20180723134644_create_reading_lists.rb
+++ db/migrate/20180723134644_create_reading_lists.rb
@@ -2,7 +2,7 @@ class CreateReadingLists < ActiveRecord::Migration[5.2]
   def change
     create_table :reading_lists do |t|
       t.references :user, foreign_key: true
-      t.references :book, foreign_key: true
+      t.references :book, foreign_key: true, uniq: true
 
       t.timestamps
     end
